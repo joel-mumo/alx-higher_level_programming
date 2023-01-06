69a63d6dbf2e9a11f02bdc4f0469bb4e56a07b46
joel-mumo <mumowambua@gmail.com>
2023-01-06 11:19:43 -0800

    Task6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div 
+    from calculator_1 import add, sub, mul, div
     import sys
 
     args_len = len(sys.argv)
     if args_len != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int(sys.argv[1])
     opr = sys.argv[2]
     b = int(sys.argv[3])
