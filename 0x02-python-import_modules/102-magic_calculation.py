1162ab82db8e1cfab065caadebf7dd63daf7eb6b
joel-mumo <mumowambua@gmail.com>
2023-01-06 11:32:53 -0800

    Task8. ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, 
     if a < b:
-        y = = add(a, b)
+        y = add(a, b)
         for i in range(4, 6):
             y = add(y, i)
         return y
