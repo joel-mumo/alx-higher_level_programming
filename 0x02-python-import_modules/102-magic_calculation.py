3ddc902dca53195647c4cd5f06d8378fbd26a5ae
joel-mumo <mumowambua@gmail.com>
2023-01-06 11:31:25 -0800

    Task8. ByteCode -> Python #3

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, 
+    if a < b:
+        y = = add(a, b)
+        for i in range(4, 6):
+            y = add(y, i)
+        return y
+    else:
+        return sub(a, b)
