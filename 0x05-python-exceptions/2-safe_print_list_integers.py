dfa42bac4375489c89ae22e9174656f0c22fee6a
joel-mumo <mumowambua@gmail.com>
2023-01-23 05:25:51 -0800

    Fixed a typo

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     r = 0
-    for i range(x):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
             r += 1
