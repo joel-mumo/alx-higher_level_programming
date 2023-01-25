2989f188ec7d46f03a69cfbbc223a7886e5c748f
joel-mumo <mumowambua@gmail.com>
2023-01-25 05:55:37 -0800

    Corrections

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -60,8 +60,8 @@ class Node:
                 while (temp.next_node is not None and
                         temp.next_node.data < value):
                     temp = temp.next_node
-                    new.next_node = tmp.next_node
-                    tmp.next_node = new
+                    new.next_node = temp.next_node
+                    temp.next_node = new
 
         def __str__(self):
             """Define the print behavior of a SinglyLinkedList"""
