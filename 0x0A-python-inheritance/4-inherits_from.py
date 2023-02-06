f299bd5277ec13c975eaba17b2374cd5fbf875b7
joel-mumo <mumowambua@gmail.com>
2023-02-06 06:16:55 -0800

    Fix stlye issue

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-"""Defines function that compares an object with an instance"""
-
+"""Defines function that compares an object with an instance."""
 
 
 def inherits_from(obj, a_class):
-   """Checks if an object is an inherited instance of a class
-   Args:
+    """Checks if an object is an inherited instance of a class.
+    Args:
         obj (any): Object to check
         a_class (type): Class to match the type of obj to
     Returns:
         True if obj is an inherited instance of a_class
         Otherwise, false
     """
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
