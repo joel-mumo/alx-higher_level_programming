1506d6c5df6e13f804ace8a23d7bb21142c3ccc4
joel-mumo <mumowambua@gmail.com>
2023-02-07 06:40:01 -0800

    a class Student that defines a student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -19,5 +19,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """Gets a dictionary representation of a Student instance"""
-        return {s: v for s, v in self.__dict__.items() if s in attrs}
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
     return self.__dict__
